dfd9448a0f9d56df772ce71422b38e73854c1f94
SoftSquad <biuro@softsquad.pl>
2020-10-13 20:19:03 +0200

    products

---
+++ database/migrations/2020_10_13_180432_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->integer('category_id')->default(1);
+            $table->text('content');
+            $table->boolean('is_activated')->default(1);
+            $table->string('locale')->default('pl');
+            $table->json('colors_ids')->nullable();
+            $table->json('sizes_ids')->nullable();
+            $table->json('shipments_ids')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
